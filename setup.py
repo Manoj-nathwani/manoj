fff48aa4f72d944653e3813bc2cc0a0c5d3d3083
Manoj Nathwani <manoj_1993@hotmail.com>
2016-03-09 16:12:30 +0000

    update

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from distutils.core import setup
 setup(
   name = 'manoj',
-  packages = ['manoj'], # this must be the same as the name above
-  version = '0.1.1',
+  packages = ['manoj'],
+  version = '2.0',
   description = 'A random test lib',
   author = 'Manoj Nathwani',
   url = 'https://github.com/Manoj-nathwani/pip-install-manoj',
