b6344488ee550f2017a8a137cf0bb65f5352e025
Manoj Nathwani <manoj_1993@hotmail.com>
2016-03-09 13:53:47 +0000

    update

---
+++ manoj/projects.py
@@ -0,0 +1,9 @@
+class Projects:
+    def __init__(self):
+        self.all_projects = ['project 1', 'project 2', 'project 3']
+
+
+    def print_projects(self):
+        print('Printing projects...')
+        for project in self.all_projects:
+            print(project)
