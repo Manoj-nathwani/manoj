74e0c648cab57507c30c359f5ef5095933b2b121
Manoj Nathwani <manoj_1993@hotmail.com>
2016-03-11 09:52:50 +0000

    update

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@ from setuptools import setup
 setup(
     name='manoj',
     version='2.0.0',
-    description='TBC',
     url='https://github.com/Manoj-nathwani/pip-install-manoj',
     author='Manoj Nathwani',
 )
